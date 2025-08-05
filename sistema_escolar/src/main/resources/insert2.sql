5b19c8f2583608e9e4dcc93c4920630da7d2eddc
Gustavo Henrique <guazambujinha@gmail.com>
2025-08-05 16:35:51 -0300

    Migração para o Postegres

---
+++ sistema_escolar/src/main/resources/insert2.sql
@@ -0,0 +1,22 @@
+-- PROFESSORES
+
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (1, 'Leticia Ribeiro', 'leticia.ribeiro@escola.com', 'Matemática');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (2, 'Pedro Barbosa', 'pedro.barbosa@escola.com', 'Português');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (3, 'Beatriz Lima', 'beatriz.lima@escola.com', 'História');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (4, 'Eduardo Almeida', 'eduardo.almeida@escola.com', 'Geografia');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (5, 'Pedro Pereira', 'pedro.pereira@escola.com', 'Física');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (6, 'Matheus Lima', 'matheus.lima@escola.com', 'Química');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (7, 'Ana Moura', 'ana.moura@escola.com', 'Biologia');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (8, 'Julia Ferreira', 'julia.ferreira@escola.com', 'Língua Inglesa');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (9, 'Diego Monteiro', 'diego.monteiro@escola.com', 'Língua Espanhola');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (10, 'Sofia Nascimento', 'sofia.nascimento@escola.com', 'Educação Física');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (11, 'Julia Araujo', 'julia.araujo@escola.com', 'Filosofia');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (12, 'Ana Silva', 'ana.silva@escola.com', 'Sociologia');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (13, 'Leonardo Santos', 'leonardo.santos@escola.com', 'Artes');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (14, 'Julia Mendes', 'julia.mendes@escola.com', 'Literatura');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (15, 'Bruna Martins', 'bruna.martins@escola.com', 'Redação');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (16, 'Daniel Gomes', 'daniel.gomes@escola.com', 'Ciências');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (17, 'Lucas Lima', 'lucas.lima@escola.com', 'Ensino Religioso');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (18, 'Pedro Ferreira', 'pedro.ferreira@escola.com', 'Pedagogia');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (19, 'Pedro Souza', 'pedro.souza@escola.com', 'Pedagogia');
+INSERT INTO tb_professor (id,nome,email,formacao) VALUES (20, 'Camila Pereira', 'camila.pereira@escola.com', 'Pedagocia');
